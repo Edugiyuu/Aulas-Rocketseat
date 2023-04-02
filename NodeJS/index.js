e34f434cc1e38b75c9e291be9ce0a2bdba62de9b
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-04-01 23:37:54 -0300

    TImers

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -3,7 +3,27 @@ const questions = [
     "O que me deixou feliz hoje?"
 ]
 const ask = (index = 0) => {
-    process.stdout.write(questions[index]+ '\n\n')
+    process.stdout.write(questions[index]+ ' > ')
 }
 
 ask()
+
+const answers = []
+process.stdin.on("data",data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    }else{
+        process.exit()
+    }
+})
+
+process.on('exit',() =>{
+    console.log(`
+    O que aprendi hoje?
+    ${answers[0]}
+
+    "O que me deixou feliz hoje?"
+    ${answers[1]}
+    `);
+})
